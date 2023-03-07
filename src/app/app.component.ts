90f01e256e0fce9a4a0c815a1b7e199060cc7353
John Doe <79935610+NfolZordo@users.noreply.github.com>
2023-03-07 23:01:43 +0200

    q

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
-import {products as data} from './data/products';
+import { ProductsService } from './services/products.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Course Work Third Year';
+  // products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]>
+  term = ''
+  constructor(private productsService: ProductsService) {
+  }
 
-  products: IProduct[] = data
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(()=> this.loading = false)
+    )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './component/product/product.component';
+import { ProductComponent } from './components/product/product.component';
+import {HttpClientModule} from '@angular/common/http';
+import { GlobalErrorComponent } from './components/global-error/global-error.component'
+import { FormsModule } from '@angular/forms';
+import { FilterProductsPipe } from './pipes/filter-products.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    GlobalErrorComponent,
+    FilterProductsPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
